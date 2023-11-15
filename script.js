f14371e35d0d09f23f54a55c5e0e075685440d4f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-15 10:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your JS code here. If required.
 function daysOfYear(year) {
-	if(year % 4 === 0){
-		alert(366);
-	}else{
-	   alert(365);
-	}   
+	if (year % 400 === 0) {
+		return 366;
+	} else if (year % 100 === 0){
+		return 365;
+	}  else if(year % 4 === 0){
+		return 366;
+	} else {
+		return 365;
+	}
 }
 const year = parseInt(prompt("Enter a year"));
 daysOfYear(year);
